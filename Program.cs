8478fbcb4bdb97e8bd10e8a1d099754fdd23fd17
A-Balan <abalan0326@gmail.com>
2023-09-23 00:12:30 -0400

    changed builder.services migrationshistory schema name

--- Program.cs
+++ Program.cs
@@ -9,9 +9,15 @@ using TheBugTracker.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//  options.UseNpgsql(connectionString));
+var connectionString = DataUtility.GetConnectionString(builder.Configuration) ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(connectionString));
+    options.UseNpgsql(connectionString, o => o.MigrationsHistoryTable(tableName: "__EFMigrationHistory",
+    schema: "TheBugTracker")));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 
