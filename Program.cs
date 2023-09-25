6dac7fdea60d366604baf4b17eb07938f13ee66f
A-Balan <abalan0326@gmail.com>
2023-09-25 11:05:35 -0400

    datat utility

--- Program.cs
+++ Program.cs
@@ -15,8 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = DataUtility.GetConnectionString(builder.Configuration) ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(connectionString, o => o.MigrationsHistoryTable(tableName: "_EFMigrationHistory",
-    schema: "TheBugTracker")));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
